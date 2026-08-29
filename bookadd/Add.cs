dcaedeaa4022ef52c53885ae932a8baa9eeaa5e8
M <YM@294.com>
2026-08-29 18:01:20 +0800

    bookadd

---
+++ bookadd/Add.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormsApp8
+{
+    public partial class Add : UserControl
+    {
+        public Add(string bookname, string author,double price,string label)
+        {
+            InitializeComponent();
+            input1.Text = bookname;
+            input2.Text = author;
+            input3.Text = price.ToString();
+            input4.Text = label;
+        }
+        public Add()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ bookadd/Form2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using WinFormsApp8;
+
+namespace WinFormsApp8
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string bookname = textBox1.Text;
+            string author = textBox2.Text;
+            double price = double.Parse(textBox3.Text);
+            string label = textBox4.Text;
+           Add bookAdd= new Add(bookname, author, price, label);
+            flowLayoutPanel1.Controls.Add(bookAdd);
+
+        }
+    }
+}
