25bc80c00762f01d31c772a80cad79d8b2ac0433
M <YM@294.com>
2026-09-01 11:05:16 +0800

    bookedit

---
+++ BookEdit.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Text.Json;
+using System.Windows.Forms;
+//using WinFormsApp9.book;
+
+namespace WinFormsApp9.book
+{
+    public partial class BookEdit : Form
+    {
+        public BookEdit()
+        {
+            InitializeComponent();
+        }
+        private string editid;
+        public BookEdit(string id)
+        {
+            InitializeComponent();
+            editid= id;
+            // 根据id查找数据
+            //MessageBox.Show(id); 
+            // 回显  ====> 根据id 查找到 对应的书籍数据 ==> 显示在界面中(界面使用UCBook)
+            //  编辑按钮, 修改
+            control1.SendData += EditBook;
+        }
+        
+        public void EditBook(Bookinfo id)
+        {
+            List<Bookinfo> books = new();
+            string jsonstr;
+            if (File.Exists("./book.json"))
+            {
+                //读文件，反序列化
+                jsonstr = File.ReadAllText("./book.json");
+                books = JsonSerializer.Deserialize<List<Bookinfo>>(jsonstr);
+            }
+           Bookinfo bookid= books.Find(item => item.Id == editid);
+            if (bookid == null)
+            {
+                MessageBox.Show("不存在");
+                this.Close();
+                return;
+            
+            }
+            int index = books.FindIndex(b => b.Id == editid);
+            books[index] = new Bookinfo()
+            {
+                Id = editid,             
+                Name = books[index].Name,       
+                Author = books[index].Author,
+                Price = books[index].Price,
+                BookLabel = books[index].BookLabel,
+                IsBorrow =  books[index].IsBorrow, 
+            };
+            jsonstr = JsonSerializer.Serialize(books,
+                new JsonSerializerOptions()
+                {
+                    WriteIndented = true,
+                    AllowTrailingCommas = true,
+                    Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+                });
+            File.WriteAllText("./book.json",jsonstr);
+            MessageBox.Show("编辑成功");
+            this.Close();
+        }
+    }
+}
